3f63129e13966ee5e6a8800520fc5d8d48844ba5
banzaoceane40-cloud <banzaoceane40@gmail.com>
2026-05-28 13:56:49 +0200

    variables created

--- app/src/main/java/za/ac/iie/mathsapp/MainActivity.kt
+++ app/src/main/java/za/ac/iie/mathsapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package za.ac.iie.mathsapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var editBirthday = findViewById<EditText>(R.id.birthdayTextInput)
+        var buttonCalculate = findViewById<Button>(R.id.submitButton)
+        var texTotal = findViewById<TextView>(R.id.textNum)
+
+        buttonCalculate.setOnClickListener{
+            var birthday = editBirthday.text.toString().toInt()
+            var total = calculateBirthday(birthday)
+            total.text = "Your total: $total"
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
